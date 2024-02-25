de9e6c430727f1c73c9dfef09a45304241d17c75
dennovik <161193274+dennovik@users.noreply.github.com>
2024-02-25 14:26:14 +0300

    Add files via upload

---
+++ 2.1.c
@@ -0,0 +1,17 @@
+
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("   *   \n");
+	printf("  ***  \n");
+	printf(" ***** \n");
+	printf("*******\n");
+	printf(" HH HH \n");
+	printf(" zzzzz \n");
+	
+	return 0;
+}
+
